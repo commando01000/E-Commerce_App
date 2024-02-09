ff20e7c59523b5ef9ed01e83d07d08e299bd1e76
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-09 19:45:44 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Navbar/Navbar.component';
+import { FooterComponent } from './Footer/Footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavbarComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
