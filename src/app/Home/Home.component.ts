e28ba1759322d44ba6d998a6585bd2219065870d
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-12 21:03:43 +0200

    added products

--- src/app/Home/Home.component.ts
+++ src/app/Home/Home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../Services/Products.service';
+import { Product } from '../interfaces/Product';
 
 @Component({
   selector: 'app-Home',
   templateUrl: './Home.component.html',
-  styleUrls: ['./Home.component.css']
+  styleUrls: ['./Home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  products: Product[] = [];
+  constructor(private _productsService: ProductsService) {
+    this._productsService.getAllProducts().subscribe({
+      next: (results) => {
+        console.log(results.data);
+        this.products = results.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('completed !');
+      },
+    });
   }
 
+  ngOnInit() {}
 }
