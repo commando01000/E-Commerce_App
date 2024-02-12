ba60368f227f0a3dcdf78084b1992d44374d15f7
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-12 21:26:57 +0200

    .

--- src/app/Home/Home.component.ts
+++ src/app/Home/Home.component.ts
@@ -9,11 +9,13 @@ import { Product } from '../interfaces/Product';
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
+  isLoading: boolean = true;
   constructor(private _productsService: ProductsService) {
     this._productsService.getAllProducts().subscribe({
       next: (results) => {
         console.log(results.data);
         this.products = results.data;
+        this.isLoading = false;
       },
       error: (err) => {
         console.log(err);
