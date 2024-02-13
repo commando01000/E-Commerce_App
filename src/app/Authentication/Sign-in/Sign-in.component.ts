287c5b649c507cba73cd223e8d0a27c5a3c9fb98
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-13 15:39:32 +0200

    sign in goes very well

--- src/app/Authentication/Sign-in/Sign-in.component.ts
+++ src/app/Authentication/Sign-in/Sign-in.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SignInService } from '../../Services/Sign-In.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-Sign-in',
   templateUrl: './Sign-in.component.html',
-  styleUrls: ['./Sign-in.component.css']
+  styleUrls: ['./Sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean = false;
 
-  ngOnInit() {
+  constructor(private _SignIn:SignInService, private _Route:Router) {}
+
+  ngOnInit() {}
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+      ),
+    ]),
+  });
+
+  onSubmit(loginForm: any) {
+    console.log(loginForm);
+    this.isLoading = true;
+    if (loginForm.valid) {
+      this._SignIn.validateUser(loginForm.value).subscribe({
+        next: (response) => {
+          console.log(response);
+          this._Route.navigate(['home']);
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.log(err.errors.message);
+          this.isLoading = false;
+        },
+        complete: () => {
+          console.log('completed !');
+          this.isLoading = false;
+        },
+      })
+    }
   }
-
 }
